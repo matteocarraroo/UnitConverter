7297bb532f4a01c7deb183c68b9f181538931a6a
matteo.carraro <matteo.carraro@student.h-is.com>
2022-10-12 12:17:31 +0200

    Added temperature conversion

--- main.py
+++ main.py
@@ -26,12 +26,21 @@ def convert_metric(unit_from, unit_to, value1):
     if unit_from == "km" and unit_to == "m": calculation = (value1 * 1000)
     return calculation
 
+def convert_temperature(unit_from, unit_to, value1):
+    if unit_from == "celsius" and unit_to == "fahrenheit": calculation = ((value1 * 9 / 5) + 32)
+    if unit_from == "celsius" and unit_to == "kelvin": calculation = (value1 + 273.15)
+    if unit_from == "fahrenheit" and unit_to == "celsius": calculation = ((value1 - 32) * 5/9)
+    if unit_from == "fahrenheit" and unit_to == "kelvin": calculation = ((value1 - 32) * 5/9 + 273.15)
+    if unit_from == "kelvin" and unit_to == "celsius": calculation = (value1 - 273.15)
+    if unit_from == "kelvin" and unit_to == "fahrenheit": calculation = ((value1 - 273.15) * 9/5 + 32)
+    return calculation
 
-available_units = ["nm", "mm", "cm", "m", "km"]
+
+available_units = ["nm", "mm", "cm", "m", "km", ""]
 unit_from = str(input(f"What unit would you like to convert from? (Please choose between {available_units}): "))
 available_units.remove(unit_from)
 unit_to = str(input(f"What unit would you like to convert to? (Please choose between {available_units}: "))
 value1 = float(input(f"Choose a value in {unit_from} to convert from: "))
 
-result = convert_metric(unit_from, unit_to, value1)
+result = convert_temperature(unit_from, unit_to, value1)
 print(f"The result is {result}{unit_to}")
