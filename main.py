bcac4753d5c002027ee8dfac69de454e5768302b
matteo.carraro <matteo.carraro@student.h-is.com>
2022-11-09 11:02:59 +0100

    Test function integration - non functional

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # Title to user
 print("This program converts units.")
+# Importing tkinter
+import tkinter as tk
 
 
 # Function for metric
-def convert_metric(unit_from, unit_to, value1):
+def convert(unit_from, unit_to, value1):
     if unit_from == "mm" and unit_to == "cm": calculation = (value1 / 10)
     if unit_from == "mm" and unit_to == "m": calculation = (value1 / 1000)
     if unit_from == "mm" and unit_to == "km": calculation = (value1 / 1e+6)
@@ -24,104 +26,149 @@ def convert_metric(unit_from, unit_to, value1):
     if unit_from == "km" and unit_to == "mm": calculation = (value1 * 1e+6)
     if unit_from == "km" and unit_to == "cm": calculation = (value1 * 100000)
     if unit_from == "km" and unit_to == "m": calculation = (value1 * 1000)
-    return calculation
-
-# Function for temperature
-def convert_temperature(unit_from, unit_to, value1):
     if unit_from == "celsius" and unit_to == "fahrenheit": calculation = ((value1 * 9 / 5) + 32)
     if unit_from == "celsius" and unit_to == "kelvin": calculation = (value1 + 273.15)
     if unit_from == "fahrenheit" and unit_to == "celsius": calculation = ((value1 - 32) * 5 / 9)
     if unit_from == "fahrenheit" and unit_to == "kelvin": calculation = ((value1 - 32) * 5 / 9 + 273.15)
     if unit_from == "kelvin" and unit_to == "celsius": calculation = (value1 - 273.15)
     if unit_from == "kelvin" and unit_to == "fahrenheit": calculation = ((value1 - 273.15) * 9 / 5 + 32)
-    return calculation
-
-# Function for volume
-def convert_volume(unit_from, unit_to, value1):
     if unit_from == "cm3" and unit_to == "m3": calculation = (value1 / 1e+6)
     if unit_from == "cm3" and unit_to == "km3": calculation = (value1 / 1e+15)
     if unit_from == "m3" and unit_to == "cm3": calculation = (value1 * 1e+6)
     if unit_from == "m3" and unit_to == "km3": calculation = (value1 / 1e+9)
     if unit_from == "km3" and unit_to == "m3": calculation = (value1 * 1e+9)
-    if unit_from == "km3" and unit_to == "cm3": calculation = (value1 * 1e+15)
+    if unit_from == "km3" and unit_to == "cm3":
+        calculation = (value1 * 1e+15)
+    else:
+        return None
     return calculation
 
-#Array to keep track of unit selected
+
+# Initializing tkinter window components
+
+# Creating window variable
+root = tk.Tk()
+# Scale value for integrating GUI program later
+scale_value = tk.StringVar()
+# Creating window with fixed not resizable size
+root.title("Converter")
+root.geometry("500x300")
+root.resizable(True, True)
+
+unit_from = tk.StringVar()
+unit_from.set("Unit from")
+unit_to = tk.StringVar()
+unit_to.set("Unit to")
+
+go_button = tk.Button(root)
+input_output = tk.Entry(root)
+input_output.insert("end", "Enter a value...")
+value1 = input_output.get()
+converted = tk.Label(root, text=convert(unit_from, unit_to, value1))
+go_button.config(text="Convert!", command=lambda: [convert(unit_from, unit_to, value1), print()])
+
+# Dictionaries to display correct name on drop-down
+met_unit = {
+    "Nanometers": "nm",
+    "Millimeters": "mm",
+    "Centimeters": "cm",
+    "Meters": "m",
+    "Kilometers": "km"
+}
+
+temp_unit = {
+    "Celsius": "celsius",
+    "Fahrenheit": "fahrenheit",
+    "Kelvin": "kelvin",
+}
+
+vol_unit = {
+    "Centimeters cubed": "cm3",
+    "Meters cubed": "m3",
+    "Kilometers cubed": "km3"
+}
+
+met_drop_to = tk.OptionMenu(root, unit_to, *met_unit)
+temp_drop_to = tk.OptionMenu(root, unit_to, *temp_unit)
+vol_drop_to = tk.OptionMenu(root, unit_to, *vol_unit)
+met_drop_from = tk.OptionMenu(root, unit_from, *met_unit)
+temp_drop_from = tk.OptionMenu(root, unit_from, *temp_unit)
+vol_drop_from = tk.OptionMenu(root, unit_from, *vol_unit)
+
+
+def gobutton():
+    go_button.place(relx=0.5, rely=0.7)
+
+
+def entrybox():
+    input_output.place(relx=0.5, rely=0.5)
+
+
+def vol_clicked_from():
+    vol_drop_from.place(relx=0.10, rely=0.6)
+
+
+def temp_clicked_from():
+    temp_drop_from.place(relx=0.10, rely=0.6)
+
+
+def met_clicked_from():
+    met_drop_from.place(relx=0.10, rely=0.6)
+
+
+def met_clicked_to():
+    met_drop_to.place(relx=0.10, rely=0.4)
+
+
+def temp_clicked_to():
+    temp_drop_to.place(relx=0.10, rely=0.4)
+
+
+def vol_clicked_to():
+    vol_drop_to.place(relx=0.10, rely=0.4)
+
+
+# Array to keep track of unit selected
 
 available_units_metric = ["nm", "mm", "cm", "m", "km"]
 available_units_temp = ["celsius", "fahrenheit", "kelvin"]
 available_units_volume = ["cm3", "m3", "km3"]
 
-#User chooses scale (temp, vol, metric
-scale = str(input(f"Would you like to convert from/to temperature, metric or volume? "))
+# User chooses scale (temp, vol, metric
+scale = scale_value
 
-#If statements to check if units are in bounds
-#Temperature if statement
-if scale == "temperature":
-    #Ask user unit to convert from
-    unit_from = input(f"What unit would you like to convert from? (Please choose between {available_units_temp}): ")
-    #Check value is in bounds
-    if unit_from == str and unit_from in available_units_temp:
-        #Remove unit chosen
-        available_units_temp.remove(unit_from)
-        #Ask user unit to convert to
-        unit_to = input(f"What unit would you like to convert to? (Please choose between {available_units_temp}: ")
-        #Check value is in bounds
-        if unit_to == str and unit_to in available_units_temp:
-            #Ask user value to convert
-            value1 = input(f"Choose a value in {unit_from} to convert from: ")
-            #Check value is in bounds
-            if value1 == int or value1 == float:
-                #Call calculation function
-                result = convert_temperature(unit_from, unit_to, value1)
-                #Output result
-                print(f"The result is {result} degrees {unit_to}")
-            else:
-                print ("Choose a numeric value.")
-        else:
-            print("Please choose from the available units.")
-    else:
-        print("Please choose from the available units.")
+# Creating a label to prompt user to choose unit
+tk.Label(root,
+         text="""Choose a 
+scale of units:""",
+         justify=tk.CENTER).pack()
 
-#Metric if loop, procedure is the same as above
-if scale == "metric":
-    unit_from = input(f"What unit would you like to convert from? (Please choose between {available_units_metric}): ")
-    if unit_from == str and unit_from in available_units_metric:
-        available_units_metric.remove(unit_from)
-        unit_to = input(f"What unit would you like to convert to? (Please choose between {available_units_metric}: ")
-        if unit_to == str and unit_to in available_units_metric:
-            value1 = input(f"Choose a value in {unit_from} to convert from: ")
-            if value1 == int or value1 == float:
-                result = convert_metric(unit_from, unit_to, value1)
-                print(f"The result is {result}{unit_to}")
-            else:
-                print ("Choose a numeric value.")
-        else:
-            print("Please choose from the available units.")
-    else:
-        print("Please choose from the available units.")
+# Creating 3 radiobuttons
+tk.Radiobutton(root,
+               text="Temperature",
+               variable=scale_value,
+               # A lambda function is a small anonymous function
+               command=lambda: [go_button.place_forget(), gobutton(), input_output.place_forget(), entrybox(),
+                                vol_drop_to.place_forget(), met_drop_to.place_forget(), met_drop_from.place_forget(),
+                                vol_drop_from.place_forget(), temp_clicked_to(), temp_clicked_from()],
+               value="temperature").place(relx=0.1, rely=0.2)
+tk.Radiobutton(root,
+               text="Volume",
+               variable=scale_value,
+               command=lambda: [go_button.place_forget(), gobutton(), input_output.place_forget(), entrybox(),
+                                met_drop_to.place_forget(), temp_drop_to.place_forget(), met_drop_from.place_forget(),
+                                temp_drop_from.place_forget(), vol_clicked_from(), vol_clicked_to()],
+               value="volume").place(relx=0.45, rely=0.2)
+tk.Radiobutton(root,
+               text="Metric",
+               variable=scale_value,
+               command=lambda: [go_button.place_forget(), gobutton(), input_output.place_forget(), entrybox(),
+                                vol_drop_to.place_forget(), temp_drop_to.place_forget(), vol_drop_from.place_forget(),
+                                temp_drop_from.place_forget(), met_clicked_to(), met_clicked_from()],
+               value="metric").place(relx=0.75, rely=0.2)
 
-#Volume if loop, procedure is the same as above
-if scale == "volume":
-    unit_from = input(f"What unit would you like to convert from? (Please choose between {available_units_volume}): ")
-    if unit_from == str and unit_from in available_units_volume:
-        available_units_volume.remove(unit_from)
-        unit_to = input(f"What unit would you like to convert to? (Please choose between {available_units_volume}: ")
-        if unit_to == str and unit_to in available_units_volume:
-            value1 = input(f"Choose a value in {unit_from} to convert from: ")
-            if value1 == int or value1 == float:
-                result = convert_volume(unit_from, unit_to, value1)
-                print(f"The result is {result}{unit_to}")
-            else:
-                print ("Choose a numeric value.")
-        else:
-            print("Please choose from the available units.")
-    else:
-        print("Please choose from the available units.")
-
-#Prevent user from choosing out of bound values
-else:
-    print("Please choose between 'temperature', 'metric' or 'volume'.")
-
-
-
+converted.place(rely=1, relx=0.5)
+# Window loop
+root.mainloop()
+while True:
+     print(unit_from.get())
